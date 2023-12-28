3784502fb219db24c252c25d75fed0282bedfb08
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-12-28 14:36:39 +0600

    landing.js: overly section edited.

--- src/Pages/Home/Landing.js
+++ src/Pages/Home/Landing.js
@@ -59,11 +59,11 @@ const Landing = () => {
 
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
-                <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary m-3">Download App</button>
-                    <button className="btn glass m-3">Choose A Plan</button>
+                <div className="max-w-fit">
+                    {/* <h1 className="mb-5 text-5xl font-bold">Hello there</h1> */}
+                    <p className="mb-5 text-white text-5xl">DISCOVER UNLIMITED MOVIES TV SHOWS, WEB SERIES ONLY ON PXFLIX</p>
+                    <button className="btn btn-primary m-3 font-bold">Download App</button>
+                    <button className="btn glass m-3 font-bold">Choose A Plan</button>
                 </div>
             </div>
         </div>
