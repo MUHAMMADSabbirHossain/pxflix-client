f75bbffb7bdbc54397d53290ebc8a1ba3561518c
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-12-28 18:23:33 +0600

    HomeTvShows.js: created, added into home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Landing from './Landing';
 import HomeFreeMovies from './HomeFreeMovies/HomeFreeMovies';
+import HomeTvShows from './HomeTvShows/HomeTvShows';
 
 const Home = () => {
     return (
         <div>
             <Landing></Landing>
             <HomeFreeMovies></HomeFreeMovies>
+            <HomeTvShows></HomeTvShows>
         </div>
     );
 };
