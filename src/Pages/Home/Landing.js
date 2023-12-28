8850867af2615ad3de4f2aa360fc30662175381e
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-12-28 14:16:51 +0600

    landing: setup initialized.

--- src/Pages/Home/Landing.js
+++ src/Pages/Home/Landing.js
@@ -2,8 +2,70 @@ import React from 'react';
 
 const Landing = () => {
     return (
-        <div>
-            Landing
+        <div className="hero min-h-screen" /* style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }} */>
+
+
+
+            <section class="min-h-svh">
+                <div className="carousel rounded-box ">
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Burger" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Burger" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Burger" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
+                    </div>
+                </div>
+
+                <div className="carousel carousel-end rounded-box">
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Drink" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Drink" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Drink" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Drink" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Drink" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Drink" />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Drink" />
+                    </div>
+                </div>
+            </section>
+
+
+            <div className="hero-overlay bg-opacity-60"></div>
+            <div className="hero-content text-center text-neutral-content">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <button className="btn btn-primary m-3">Download App</button>
+                    <button className="btn glass m-3">Choose A Plan</button>
+                </div>
+            </div>
         </div>
     );
 };
