c72ecd4e090ea7e958fbcaee123364f8e56a5605
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-12-28 08:32:47 +0600

    package, NotFoundPage: react router dom installed, notfoundpage linked.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 import './App.css';
 import Footer from './Components/Footer/Footer';
 import Navbar from './Components/Navbar/Navbar';
+import NotFoundPage from './Components/NotFoundPage/NotFoundPage';
 import Home from './Pages/Home/Home';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
@@ -23,7 +25,12 @@ function App() {
       </header> */}
 
       <Navbar></Navbar>
-      <Home></Home>
+      <Routes>
+        <Route path='/' element={<Home></Home>
+        }></Route>
+        <Route path='*' element={<NotFoundPage></NotFoundPage>}></Route>
+      </Routes>
+
       <Footer></Footer>
 
     </div>
