fda8386a6b867b9827a561489c0769a8d63c2b8c
MUHAMMADSabbirHossain <dustobalok000@gmail.com>
2023-12-28 15:08:46 +0600

    home.js, homefreemovies: created; added; liked

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Landing from './Landing';
+import HomeFreeMovies from './HomeFreeMovies/HomeFreeMovies';
 
 const Home = () => {
     return (
         <div>
             <Landing></Landing>
+            <HomeFreeMovies></HomeFreeMovies>
         </div>
     );
 };
---
+++ src/Pages/Home/HomeFreeMovies/HomeFreeMovies.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+const HomeFreeMovies = () => {
+    return (
+        <div className='min-h-dvh'>
+
+            {/* section title */}
+            <h2 className='text-3xl text-white py-12'>FREE MOVIES</h2>
+
+            {/* section cards */}
+            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4'>
+
+                <div className="mx-auto card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Shoes!
+                            <div className="badge badge-secondary">NEW</div>
+                        </h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">Fashion</div>
+                            <div className="badge badge-outline">Products</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mx-auto card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Shoes!
+                            <div className="badge badge-secondary">NEW</div>
+                        </h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">Fashion</div>
+                            <div className="badge badge-outline">Products</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mx-auto card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Shoes!
+                            <div className="badge badge-secondary">NEW</div>
+                        </h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">Fashion</div>
+                            <div className="badge badge-outline">Products</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="mx-auto card w-96 bg-base-100 shadow-xl">
+                    <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">
+                            Shoes!
+                            <div className="badge badge-secondary">NEW</div>
+                        </h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div className="card-actions justify-end">
+                            <div className="badge badge-outline">Fashion</div>
+                            <div className="badge badge-outline">Products</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default HomeFreeMovies;
